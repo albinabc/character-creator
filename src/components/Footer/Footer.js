07c90ba1447fe42b872ad071184570d61ff63e1a
Albin Jonfelt <albin@abclabs.se>
2024-09-14 09:23:58 +0200

    my attempt

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      All character assets taken from{' '}
+      All character assets taken from{" "}
       <a
         href="https://www.openpeeps.com/"
         target="_blank"
+        className={styles.a}
         rel="noopener noreferrer"
       >
         Open Peeps
